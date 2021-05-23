fd17a5a6d2dc249d618fa66a5703d09341cd9592
Jonas Waeber <jonas.waeber@unibas.ch>
2021-05-23 20:26:41 +0200

    add article types.

--- python.py
+++ python.py
@@ -5,9 +5,11 @@ if __name__ == '__main__':
     with open('input.csv', 'r') as fp:
         values = dict()
         values['values'] = dict()
+        value = 'value'
+        count = 1
         for line in fp:
-            name = line.strip().capitalize()
-            values['values'][name] = name
-
+            name = line.strip()
+            values['values'][f'{value}{count}'] = name
+            count += 1
     with open('output.json', 'w') as fp:
         json.dump(values, fp, ensure_ascii=False, indent='    ')

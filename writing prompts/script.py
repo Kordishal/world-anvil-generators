4ed3f9c58762c48952d137fc26a2c84d24053e23
Jonas Waeber <jonas.waeber@unibas.ch>
2021-05-29 15:25:54 +0200

    Basic template selection for prompt start.

--- writing prompts/script.py
+++ writing prompts/script.py
@@ -8,13 +8,28 @@ def write_file(data: Any, filename: str):
         json.dump(data, fp, ensure_ascii=False, indent='    ')
 
 
+def create_payload_dict(variable: str):
+    router = dict()
+    router[variable] = dict()
+    return router, router[variable]
+
+
 if __name__ == '__main__':
-
-    with open('new-article-links.csv', 'r') as fp:
+    with open('article-template-core.csv', 'r') as fp:
         reader = csv.reader(fp, dialect='unix')
-        router = dict()
-        router['new-article-link'] = dict()
-        for row in reader:
-            router['new-article-link'][row[0]] = row[1]
+        select_template_generator, select_template = create_payload_dict('values')
+        new_article_link_router, new_article_link = create_payload_dict('new-article-link')
+        prompt_intro_router, prompt_intro = create_payload_dict('prompt-intro')
 
-    write_file(router, 'new-article-link-transformer')
+        count = 1
+        for row in reader:
+            if count == 1:
+                count += 1
+                continue
+            select_template[row[0]] = row[1]
+            new_article_link[row[0]] = row[2]
+            prompt_intro[row[0]] = row[3]
+
+    write_file(select_template_generator, 'select-template-generator')
+    write_file(new_article_link_router, 'new-article-link-transformer')
+    write_file(prompt_intro_router, 'prompt-intro-transformer')

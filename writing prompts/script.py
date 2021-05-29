647811e97b0ae9b768b5d3bc39d37846fca7f292
Jonas Waeber <jonas.waeber@unibas.ch>
2021-05-29 18:26:35 +0200

    ADd sentence connecter Add target article transformer

--- writing prompts/script.py
+++ writing prompts/script.py
@@ -20,6 +20,8 @@ if __name__ == '__main__':
         select_template_generator, select_template = create_payload_dict('values')
         new_article_link_router, new_article_link = create_payload_dict('new-article-link')
         prompt_intro_router, prompt_intro = create_payload_dict('prompt-intro')
+        sentence_connector_router, sentence_connector = create_payload_dict('sentence-connector')
+        target_article_router, target_article = create_payload_dict('target-article')
 
         count = 1
         for row in reader:
@@ -29,7 +31,11 @@ if __name__ == '__main__':
             select_template[row[0]] = row[1]
             new_article_link[row[0]] = row[2]
             prompt_intro[row[0]] = row[3]
+            sentence_connector[row[0]] = row[4]
+            target_article[row[0]] = f'[generator:{row[0]}-article-28world29-soullink|{"{}"}|relation-target-1]'
 
     write_file(select_template_generator, 'select-template-generator')
     write_file(new_article_link_router, 'new-article-link-transformer')
     write_file(prompt_intro_router, 'prompt-intro-transformer')
+    write_file(sentence_connector_router, 'sentence-connector-transformer')
+    write_file(target_article_router, 'target-article-transformer')

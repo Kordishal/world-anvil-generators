4ecf690d41b75ddda3dfa233be0abceaae1f8876
Jonas Waeber <jonas.waeber@unibas.ch>
2021-08-28 09:34:28 +0200

    French names

---
+++ names/french/README.md
@@ -0,0 +1,6 @@
+## French Names
+
+### Sources
+
+- [French given names](https://en.wiktionary.org/wiki/Category:French_given_names)
+- [French surnames](https://en.wiktionary.org/wiki/Category:French_surnames)
--- scripts/unisex-names.py
+++ scripts/unisex-names.py
@@ -2,9 +2,9 @@
 
 if __name__ == '__main__':
 
-    with open('sources/female-given-names.csv') as f:
+    with open('../names/french/sources/female-given-names.csv') as f:
         female_names = f.read().split('\n')
-    with open('sources/male-given-names.csv') as m:
+    with open('../names/french/sources/male-given-names.csv') as m:
         male_names = m.read().split('\n')
 
     set_female = set(female_names)
@@ -12,4 +12,7 @@ if __name__ == '__main__':
 
     values = set_female.intersection(set_male)
     print(values)
+    with open('unisex-names.csv', 'w') as fp:
+        for value in values:
+            fp.write(value + '\n')
 
